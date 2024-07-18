0b679c4cab61e4da741f15aba4bbf2ca6fd53e4e
hung.nguyen <hung.nguyen@zien.vn>
2024-07-18 14:57:31 +0700

    first commit

---
+++ src/components/DateTimePicker.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {
+  value: number
+}
+
+function DateTimePicker({ value }: Props) {
+  return <div>DateTimePicker - {value}</div>
+}
+
+export default DateTimePicker
